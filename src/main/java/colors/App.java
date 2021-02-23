600a1cc37c07507340cc22381dd3d90fb18eb02a
Romain Tello <romain.tello@gmail.com>
2021-02-23 11:54:21 +0100

    Solve step 1

--- src/main/java/colors/App.java
+++ src/main/java/colors/App.java
@@ -1,5 +1,7 @@
 package colors;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
@@ -12,6 +14,22 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        String userInput = scanner.nextLine();
+
+        int[] numbers = new int[3];
+        if (userInput.length() == 6) {
+            for (int i = 0; i < 3; i++) {
+                int beginIndex = i * 2;
+                int endIndex = beginIndex + 2;
+
+                String numberStr = userInput.substring(beginIndex, endIndex);
+
+                numbers[i] = Integer.parseInt(numberStr, 16);
+            }
+        }
+
+        System.out.println("rgb(" + numbers[0] + ", " + numbers[1] + ", " + numbers[2] + ")");
     }
 }
